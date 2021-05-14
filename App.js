2384f7dd3bc0e6175e4d76d74d1934ee580350e7
mpmoran2 <kamokumoran@gmail.com>
2021-05-13 17:26:29 -0700

    landing page created

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bleep Beep Imma Sheep</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#C8A2C8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
